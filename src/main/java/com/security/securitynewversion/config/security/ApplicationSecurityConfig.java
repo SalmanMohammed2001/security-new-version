5d2d5d7f8132f94bd3cebe256e64ed9c5a467c79
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-27 15:11:47 +0530

    set user

--- src/main/java/com/security/securitynewversion/config/security/ApplicationSecurityConfig.java
+++ src/main/java/com/security/securitynewversion/config/security/ApplicationSecurityConfig.java
@@ -5,7 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -13,14 +19,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
 
-    @Bean
+     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
-
+         http.authorizeHttpRequests(authz -> authz
+                 .requestMatchers("/api/v1/customers/list/**").permitAll()
+                 .anyRequest().authenticated())
+                 .httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails  user1= User.builder()
+                .username("user1")
+                .password("1234")
+                .roles("CUSTOMER")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+
+
+
+
+
+
 }
