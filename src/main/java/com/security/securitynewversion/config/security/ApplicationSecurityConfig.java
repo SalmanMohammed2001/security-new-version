c399b23c9e45694b936fdd5827d28637561c3e6b
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-27 19:57:16 +0530

    Controller set user path

--- src/main/java/com/security/securitynewversion/config/security/ApplicationSecurityConfig.java
+++ src/main/java/com/security/securitynewversion/config/security/ApplicationSecurityConfig.java
@@ -1,19 +1,18 @@
 package com.security.securitynewversion.config.security;
 
-import com.security.securitynewversion.config.permission.ApplicationUserRole;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 import static com.security.securitynewversion.config.permission.ApplicationUserRole.*;
 import static org.springframework.security.config.Customizer.withDefaults;
